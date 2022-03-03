5b1e3bff2be947587948dd63face212a4be5fae0
arifsptra <arif.sptrra@gmail.com>
2022-03-03 14:56:25 +0700

    answer the 3rd case question the program looks for the average score

--- Tugas/Lab01/Kasus3.cpp
+++ Tugas/Lab01/Kasus3.cpp
@@ -15,8 +15,25 @@ int main(){
         foo = foo+A[i];
     }
 
-    // Menampilkan hasil dari variable foo dibagi n
+    // Menampilkan hasil dari variable foo dibagi dengan varibel n
     cout << (foo/n) << endl;
-    
+
     return 0;
 }
+
+// Question
+// 1. Apa output program diatas?
+// 2. Apakah ada Error? 
+// 3. Jika ada error, apa errornya? Dan apa output setelah diperbaiki?
+// 4. Berikan judul yang cocok untuk program diatas? 
+// 5. Jelaskan alur program diatas?
+
+// Answer
+// 1. Output program diatas adalah hasil dari variable foo dibagi dengan varibel n
+// 2. Ada
+// 3. Kesalahan terletak pada kondisi perulangan, yang awalnya i<=n diubah menjadi i<n. Karena index array dimuali dari 0 dan nilai awal i nya adalah 0
+// 4. Program mencari nilai rata-rata
+/* 5.   - Pertama program akan melakukan perulangan sampai nilai i tidak lebih atau sama dengan nilai variabel n yang bernilai 8
+        - Didalam perulangan ada inisialisasi variabel foo dimana nilai varibel foo akan ditambah dengan nilai varibel array index ke i. Sampai perulangan berakhir.
+        - Setelah perulangan berakhir, kemudian akan menampilkan hasil bagi dari variable foo dibagi dengan varibel n
+*/
