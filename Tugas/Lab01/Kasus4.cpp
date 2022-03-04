f5c42597450d6325b203c02ef4f5bb33c91b851f
arifsptra <arif.sptrra@gmail.com>
2022-03-04 08:30:53 +0700

    make the fourth case program error check

---
+++ Tugas/Lab01/Kasus4.cpp
@@ -0,0 +1,21 @@
+// Cek Error
+
+#include <iostream>
+
+using namespace std;
+
+void myFunction(){
+    cout >> "Alo!!" >> endl;
+}
+
+int myFunction(int x){
+    return 5 + x
+}
+
+int main(){
+    cout << "Hello World << endl;
+    cout << myFunction();
+    cout << myFunction(7);
+
+    return 0;
+}
