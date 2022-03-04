35eab03475e565f8154551260c2cb253516e6ca6
arifsptra <arif.sptrra@gmail.com>
2022-03-04 08:51:45 +0700

    provide an explanation and the location of the error from the code

--- Tugas/Lab01/Kasus4.cpp
+++ Tugas/Lab01/Kasus4.cpp
@@ -19,3 +19,9 @@ int main(){
 
     return 0;
 }
+
+// Where the error is
+// 1. di dalam fungsi pertama yang bertipe data void, karena tanda cout nya kebalik yang seharusnya <<
+// di dalam fungsi ke dua, karena setelah return kurang titik koma
+// di fungsi main di cout hello world kurang tanda petik di akhir kata
+// di dalam fungsi main untuk pemanggilan fungsi myFunction() tidak perlu menggunakan cout, karena fungsi tersebut bertipe data void dimana fungsi ini tidak mengembalikan nilai
