e54d86132df387279fdf0b47f3cf6db84051d5b6
arifsptra <arif.sptrra@gmail.com>
2022-03-07 11:36:21 +0700

    practice string operations strcat, strupr, and strlwr

--- Praktikum/03-OperasiString.cpp
+++ Praktikum/03-OperasiString.cpp
@@ -11,7 +11,18 @@ int main(){
     cout << name << endl;
 
     // strlen => Gets the number of characters from a string
-    cout << "Number of characters: " << strlen(name);
+    cout << "Number of characters: " << strlen(name) << endl;
+
+    char hobi[30] = "Saya Memiliki Hobi ";
+    char myHobi[50] = "Bermain Sepak Bola, Membaca Buku, dan Traveling";
+    // strcat => Append the source string to the end of destination string
+    cout << strcat(hobi, myHobi) << endl;
+
+    // strupr => Converts each letter of a string to capital letters
+    cout << strupr(hobi) << endl;
+
+    // strlwr => Converts each letter of a string to all lowercase
+    cout << strlwr(hobi) << endl;
 
     return 0;
 }
