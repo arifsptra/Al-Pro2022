43d8a2646317eff81d8bdba2e69706bd787c6bb8
arifsptra <arif.sptrra@gmail.com>
2022-03-07 09:47:46 +0700

    make presentations in lectures

--- Tugas/Lab01/Kasus4.cpp
+++ Tugas/Lab01/Kasus4.cpp
@@ -8,14 +8,14 @@ void myFunction(){
     cout << "Alo!!" << endl;
 }
 
-int myFunction(int x){
+int myFunction2(int x){
     return 5 + x;
 }
 
 int main(){
     cout << "Hello World" << endl;
     myFunction();
-    cout << myFunction(7);
+    cout << myFunction2(7);
 
     return 0;
 }
@@ -24,4 +24,5 @@ int main(){
 // 1. di dalam fungsi pertama yang bertipe data void, karena tanda cout nya kebalik yang seharusnya <<
 // di dalam fungsi ke dua, karena setelah return kurang titik koma
 // di fungsi main di cout hello world kurang tanda petik di akhir kata
-// di dalam fungsi main untuk pemanggilan fungsi myFunction() tidak perlu menggunakan cout, karena fungsi tersebut bertipe data void dimana fungsi ini tidak mengembalikan nilai
+// di dalam fungsi main untuk pemanggilan fungsi myFunction() tidak perlu menggunakan cout, karena fungsi tersebut tidak mengembalikan nilai 
+// nama varibel boleh sama asal mempunyai parameter yang berbeda
