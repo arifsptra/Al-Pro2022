52231af05cda413d2712e7978ebf4756bc5bbdbc
arifsptra <arif.sptrra@gmail.com>
2022-03-08 12:02:52 +0700

    practice of toupper and tolower character operations

--- Praktikum/05-LatihanOperasiKarakter.cpp
+++ Praktikum/05-LatihanOperasiKarakter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctype.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -12,10 +13,16 @@ int main(){
     // islower => Returns true(non-zero) if character is lowercase
     if(islower(karakter)){
         cout << "Karakter anda huruf kecil" << endl;
+        // toupper => Convert lowercase letters to capital
+        // cout << "Huruf kapitalnya: " << tolower(karakter) << endl;
+        printf("Huruf Kapitalnya: %c", toupper(karakter));
 
     // isupper => Returns true(non-zero) if character is a capital letter
     }else if(isupper(karakter)){
         cout << "Karakter anda huruf kapital" << endl;
+        // tolower => Convert capital letters to lowercase
+        // cout << "Huruf kecilnya: " << tolower(karakter) << endl;
+        printf("Huruf Kecilnya: %c", tolower(karakter));
     
     // isdigit => Returns true(non-zero) if character is a digit
     }else if(isdigit(karakter)){
