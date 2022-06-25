6a09fa5a53bfd6138edd66d5c64bf4b0a8ab1d4f
arifsptra <arif.sptrra@gmail.com>
2022-06-25 19:49:17 +0700

    debugging uas problem

---
+++ Tugas/LatihanUAS/debug.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class titik {
+    public:
+    int a;
+};
+
+int main(){
+    titik t;
+    titik z[1,2];
+    titik a(0, 0);
+    titik b(10, 5);
+
+    return 0;
+}
